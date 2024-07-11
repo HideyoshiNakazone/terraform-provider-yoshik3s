94f5aca3e9263831c3627c28e1f122f2b86a50c6
Vitor Hideyoshi Nakazone Batista <vitor.h.n.batista@gmail.com>
2024-07-11 01:01:54 -0300

    Updates Docs and Example Formatting

--- examples/provider/main.tf
+++ examples/provider/main.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_providers {
-        yoshik3s = {
-            source = "registry.terraform.io/HideyoshiNakazone/yoshi-k3s"
-        }
+  required_providers {
+    yoshik3s = {
+      source = "registry.terraform.io/HideyoshiNakazone/yoshi-k3s"
     }
+  }
 }
 
 
@@ -11,50 +11,50 @@ provider "yoshik3s" {}
 
 
 resource "yoshik3s_cluster" "example_cluster" {
-    name = "example-cluster"
-    token = "example_token"
-    k3s_version = "v1.30.2+k3s2"
+  name        = "example-cluster"
+  token       = "example_token"
+  k3s_version = "v1.30.2+k3s2"
 }
 
 
 resource "yoshik3s_master_node" "example_master_node" {
-    cluster = {
-        token = yoshik3s_cluster.example_cluster.token
-        k3s_version = yoshik3s_cluster.example_cluster.k3s_version
-    }
+  cluster = {
+    token       = yoshik3s_cluster.example_cluster.token
+    k3s_version = yoshik3s_cluster.example_cluster.k3s_version
+  }
 
-    node_connection = {
-        host = "localhost"
-        port = "2222"
-        user = "sshuser"
-        password = "password"
-    }
+  node_connection = {
+    host     = "localhost"
+    port     = "2222"
+    user     = "sshuser"
+    password = "password"
+  }
 
-    node_options = [
-		"--disable traefik",
-		"--node-label node_type=master",
-		"--snapshotter native",
-    ]
+  node_options = [
+    "--disable traefik",
+    "--node-label node_type=master",
+    "--snapshotter native",
+  ]
 }
 
 
 resource "yoshik3s_worker_node" "example_worker_node" {
-    master_server_address = "master_node"
+  master_server_address = "master_node"
 
-    cluster = {
-        token = yoshik3s_cluster.example_cluster.token
-        k3s_version = yoshik3s_cluster.example_cluster.k3s_version
-    }
+  cluster = {
+    token       = yoshik3s_cluster.example_cluster.token
+    k3s_version = yoshik3s_cluster.example_cluster.k3s_version
+  }
 
-    node_connection = {
-        host = "localhost"
-        port = "3333"
-        user = "sshuser"
-        password = "password"
-    }
+  node_connection = {
+    host     = "localhost"
+    port     = "3333"
+    user     = "sshuser"
+    password = "password"
+  }
 
-    node_options = [
-		"--node-label node_type=worker",
-		"--snapshotter native",
-    ]
+  node_options = [
+    "--node-label node_type=worker",
+    "--snapshotter native",
+  ]
 }
